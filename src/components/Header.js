cc9aec973468a72795c3d86640eda708caad7588
Arpeet Naik <arpeetnaik31@yahoo.com>
2025-06-05 23:52:49 -0400

    Initial commit of my expense tracker react app

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/Header.css';
+
+function Header(){
+  return (
+      <h2 className="header-bubble">Expense Tracker</h2>
+
+  );
+}
+
+export default Header;
